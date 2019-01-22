512b3229bd5dd632a3b1a4ac831d08a93b9b933f
kimhoon <innamgo@naver.com>
2019-01-22 15:55:31 +0900

    ReactNative App

---
+++ ReactNativeWebApp/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { WebView } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <WebView
+        source={{uri: 'https://www.airphilip.com'}}
+        style={{marginTop: 20}}
+      />
+    );
+  }
+}
+
